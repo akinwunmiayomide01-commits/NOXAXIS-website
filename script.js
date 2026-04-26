a64dc284beaa91d009fa548772fd299d3ba20365
Akinwunmi Ayomide <akinwunmiayomide01@gmail.com>
2026-04-26 21:17:23 +0100

    Update branding, fix logo, improve UI design

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
 menuToggle.addEventListener("click", () => {
-  if (navLinks.style.display === "flex") {
-    navLinks.style.display = "none";
-  } else {
-    navLinks.style.display = "flex";
-  }
+  navLinks.style.display =
+    navLinks.style.display === "flex" ? "none" : "flex";
 });
